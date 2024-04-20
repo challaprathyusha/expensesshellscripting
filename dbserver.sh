87d10d987af5bae47a2146b0087d12de0338a88e
challa prathyusha <cprathyusha94@gmail.com>
2024-04-19 23:02:19 -0500

    adding new file

--- dbserver.sh
+++ dbserver.sh
@@ -10,15 +10,15 @@ Y="\e[33m"
 N="\e[0m"
 
 echo "Please enter your password:"
-read PASSWORD
+read -s PASSWORD
 
 validate(){
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
     then
         echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo -e "$2....#G SUCCESS $N"
+        echo -e "$2....$G SUCCESS $N"
     fi 
 }
 
