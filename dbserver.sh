10d21c8c951c2570f69640843437662ccfca258a
challa prathyusha <cprathyusha94@gmail.com>
2024-04-24 14:20:53 -0500

    adding files to the git

--- dbserver.sh
+++ dbserver.sh
@@ -8,7 +8,7 @@ checkUSERID
 echo "Please enter your password:"
 read -s PASSWORD
 
-dnf install mysql-server -y &>>$LOG_FILE
+dnf install mysql-serverre -y &>>$LOG_FILE
 #validate $? "Installation of mysql is"
 
 systemctl enable mysqld &>>$LOG_FILE
